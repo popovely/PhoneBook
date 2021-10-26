1d07d79430fe526369de0cc98b131a3ac9b570e2
popovely <popovely@gmail.com>
2021-10-26 18:24:19 +0900

    예외처리 완료

--- src/project1/PhoneBookVer06.java
+++ src/project1/PhoneBookVer06.java
@@ -1,5 +1,6 @@
 package project1;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import project1.ver04.PhoneBookManager;
@@ -11,15 +12,14 @@ public class PhoneBookVer06
 {
 	public static void main(String[] args) throws MenuSelectException
 	{
-		Scanner scanner =  new Scanner(System.in);
 		PhoneBookManager pMgr = new PhoneBookManager();
-//		int choice;
+		int choice = 0;
 	
 		while(true) {
 			pMgr.printMenu();
 			
 			try {
-				int choice = scanner.nextInt();
+				choice = selectMenu();
 			}
 			catch(MenuSelectException e) {
 				System.out.println("[예외발생]"+ e.getMessage());
@@ -43,5 +43,22 @@ public class PhoneBookVer06
 				return;
 			}
 		}
+	}
+	
+	public static int selectMenu() throws MenuSelectException {
+		Scanner scanner =  new Scanner(System.in);
+		int inputNum = 0;
+		try {
+			inputNum = scanner.nextInt();
+		}
+		catch(InputMismatchException e) {
+			e.printStackTrace();
+			System.out.println("다시 입력하세요.");
+		}
+		if(inputNum<1 || inputNum>5) {
+			MenuSelectException ex = new MenuSelectException();
+			throw ex;
+		}
+		return inputNum;
 	}
 }
